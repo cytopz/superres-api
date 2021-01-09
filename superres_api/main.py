f56e00747e40faf74381631fbafd6e8adb0ab6c8
cytopz <cytopz@protonmail.com>
2021-01-09 10:22:15 +0700

    apply CORS middleware

--- superres_api/main.py
+++ superres_api/main.py
@@ -1,10 +1,18 @@
 from enum import IntEnum, Enum
 from fastapi import  FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from .upscaler import Upscaler
 from .image import Image
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class Scale(IntEnum):
     x2 = 2
